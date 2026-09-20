080b7762d3a84345ce24e9e8649b4f819677bd4c
Alberto La Rocca <71104@users.noreply.github.com>
2026-09-20 11:29:36 +0200

    Update dependencies.

--- src/bluesky.rs
+++ src/bluesky.rs
@@ -626,6 +626,8 @@ impl TryFrom<usize> for Scalar {
 }
 
 impl Field for Scalar {
+    type BaseField = Scalar;
+
     const MODULUS: &'static str =
         "0x7ffffffffffffffffffffffffffffffe0673ddf29e9b5547c000000000000001";
 
